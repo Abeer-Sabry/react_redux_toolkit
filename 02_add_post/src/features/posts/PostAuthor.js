29d06675f6187b98ba5d9e8fe17ee38039ae17d7
Abeer Sabry <abeersabry031@gmail.com>
2023-01-02 19:30:40 +0200

    addPosts

---
+++ 02_add_post/src/features/posts/PostAuthor.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersSlice";
+
+const PostAuthor = ({ userId }) => {
+  const users = useSelector(selectAllUsers);
+  // we map through users state to find the user which it's id equal to postAdded action payload id
+  const author = users.find(user => user.id === userId);
+  // we find it ? display it's name
+  return <span>by {author ? author.name : "unKnown author"} </span>;
+};
+
+export default PostAuthor;
